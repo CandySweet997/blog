befd836e6c9b0f1bdc7d9fa1ec3aa48d623e8215
Msi <cswit997@gmail.com>
2021-11-28 11:47:54 +0330

    start

---
+++ src/main/java/ir/arcademy/blog/modules/posts/service/PostsService.java
@@ -0,0 +1,48 @@
+package ir.arcademy.blog.modules.posts.service;
+
+
+import ir.arcademy.blog.modules.posts.model.Posts;
+import ir.arcademy.blog.modules.posts.repository.PostsRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
+
+import javax.transaction.Transactional;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+
+@Service
+public class PostsService {
+
+    private PostsRepository postsRepository;
+
+    @Autowired
+    public PostsService(PostsRepository postsRepository)  {
+
+        this.postsRepository = postsRepository;
+    }
+
+    @Transactional
+    public Posts registerPost(Posts posts) throws IOException {
+        String path = ResourceUtils.getFile("classpath:static/img").getAbsolutePath();
+        // image storage address
+        byte[] bytes = posts.getFile().getBytes();
+        String name = UUID.randomUUID() +"."+ Objects.requireNonNull(posts.getFile().getContentType()).split("/")[1];
+        Files.write(Paths.get(path + File.separator + name),bytes);
+        posts.setCover(name);
+        return this.postsRepository.save(posts);
+    }
+
+    public List<Posts> findAllPosts() {
+        return this.postsRepository.findAll();
+    }
+
+
+}
+
